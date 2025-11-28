df3d36fe03acb136fcdef8b9a2dc9eaf184aa79f
maugrhub <mauricio@gr-hub.com>
2025-11-28 13:00:29 -0500

    Enhance card display and update discount texts
    Added dynamic styling for card text overflow and updated discount messages for different languages.

--- cards-slider/nexus-cards-generations.js
+++ cards-slider/nexus-cards-generations.js
@@ -1,5 +1,24 @@
 export function generarTarjetasProductos(category, site, delegation, idSlider) {
     document.addEventListener('DOMContentLoaded', () => {
+
+        // Obtencion de color base y clases agregadas para desborde de textos
+        var textColor = document.querySelector(".txt1");
+        var mainColor = window.getComputedStyle(textColor).color;
+
+        var addStyleSlider = `
+        :is(#${idSlider})
+        >.slick-list>.slick-track>.slick-slide>div>.card h3 {
+            display: -webkit-box !important;
+            -webkit-line-clamp: 2 !important;
+            -webkit-box-orient: vertical !important;
+            overflow: hidden !important;
+        }
+        `;
+
+        const styleTag = document.createElement("style");
+        styleTag.textContent = addStyleSlider;
+        document.head.appendChild(styleTag);
+
         // Variables de idioma y moneda
         let dataLang, dataCurrency;
     
@@ -65,7 +84,7 @@ export function generarTarjetasProductos(category, site, delegation, idSlider) {
             else
                 productosFiltrados = productos.filter(product =>
                     // Filtro para filtrar igual las diferentes delegaciones
-                    product['categoria'] === dataCategoria && product['sitio'] === dataSitio && product['delegation_en'] === dataDelegation
+                    product['categoria'] === dataCategoria && product['sitio'] === dataSitio && (product['delegation_en'] === dataDelegation || product['delegation_mx'] === dataDelegation || product['delegation_fr'] === dataDelegation)
                 );
 
             // Ordenar de mayor a menor según 'priority' y tomar solo los primeros 15 elementos
@@ -144,22 +163,22 @@ export function generarTarjetasProductos(category, site, delegation, idSlider) {
                 
                 const pDiscount = document.createElement('p');
                 pDiscount.className = 'discount';
-                const discountSvg = `<svg fill="none" viewBox="0 0 16 16"><g clip-path="url(#a)" fill="#FF621D"><path d="M15.8626.1373a.4687.4687 0 0 0-.6629 0l-1.3653 1.3653a1.4 1.4 0 0 0-.265-.0953L9.3266.359a1.414 1.414 0 0 0-1.3356.37L.4112 8.2964c-.5483.5483-.5483 1.4405 0 1.9888l5.3035 5.3035c.5483.5483 1.4405.5484 1.9889 0l7.5674-7.5799c.3473-.3473.489-.859.3699-1.3355l-1.0482-4.2428a1.4 1.4 0 0 0-.0953-.265L15.8627.8002a.469.469 0 0 0-.0001-.6629m-1.1313 6.7635a.471.471 0 0 1-.1233.4451l-7.5674 7.5799a.4693.4693 0 0 1-.663 0L1.0741 9.6223a.4693.4693 0 0 1 0-.663l7.58-7.5674a.471.471 0 0 1 .445-.1233l3.9854.9839-1.1289 1.1289c-.5227-.249-1.1683-.1577-1.6003.2744-.5484.5483-.5484 1.4405 0 1.9888.2741.2742.6343.4113.9944.4113s.7202-.137.9944-.4113c.4321-.432.5234-1.0777.2744-1.6003l1.1289-1.129zm-3.0501-1.919a.4694.4694 0 0 1-.663 0 .4694.4694 0 0 1 0-.663.467.467 0 0 1 .3315-.137.467.467 0 0 1 .3314.137h.0001a.4694.4694 0 0 1 0 .663"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h16v16H0z"/></clipPath></defs></svg>`;
+                const discountSvg = `<svg fill="none" viewBox="0 0 16 16"><g clip-path="url(#a)" fill="${mainColor}"><path d="M15.8626.1373a.4687.4687 0 0 0-.6629 0l-1.3653 1.3653a1.4 1.4 0 0 0-.265-.0953L9.3266.359a1.414 1.414 0 0 0-1.3356.37L.4112 8.2964c-.5483.5483-.5483 1.4405 0 1.9888l5.3035 5.3035c.5483.5483 1.4405.5484 1.9889 0l7.5674-7.5799c.3473-.3473.489-.859.3699-1.3355l-1.0482-4.2428a1.4 1.4 0 0 0-.0953-.265L15.8627.8002a.469.469 0 0 0-.0001-.6629m-1.1313 6.7635a.471.471 0 0 1-.1233.4451l-7.5674 7.5799a.4693.4693 0 0 1-.663 0L1.0741 9.6223a.4693.4693 0 0 1 0-.663l7.58-7.5674a.471.471 0 0 1 .445-.1233l3.9854.9839-1.1289 1.1289c-.5227-.249-1.1683-.1577-1.6003.2744-.5484.5483-.5484 1.4405 0 1.9888.2741.2742.6343.4113.9944.4113s.7202-.137.9944-.4113c.4321-.432.5234-1.0777.2744-1.6003l1.1289-1.129zm-3.0501-1.919a.4694.4694 0 0 1-.663 0 .4694.4694 0 0 1 0-.663.467.467 0 0 1 .3315-.137.467.467 0 0 1 .3314.137h.0001a.4694.4694 0 0 1 0 .663"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h16v16H0z"/></clipPath></defs></svg>`;
                 let discountText = "";
                 if (!isOpaque) {
                     if (dataLang === 'es') {
-                        discountText = " Hasta un 5% de descuento al registrarte";
+                        discountText = " Regístrate y obtén un 5% de descuento adicional";
                     } else if (dataLang === 'fr') {
-                        discountText = "Inscrivez-vous et obtenez 5 % de réduction en plus";
+                        discountText = " Inscrivez-vous et obtenez 5 % de réduction en plus";
                     } else {
-                        discountText = "Sign up and get an extra 5% off your purchase";
+                        discountText = " Sign up and get an extra 5% off your purchase";
                     }
                 } else {
                     const discountValue = product['desc_final_opaco'];
                     if (dataLang === 'es') {
-                        discountText = ` Disfruta hasta ${discountValue}% de descuento.`;
+                        discountText = ` Disfruta hasta ${discountValue}% de descuento adicional.`;
                     } else if (dataLang === 'fr') {
-                        discountText = ` Profitez jusqu'à ${discountValue}% de réduction.`;
+                        discountText = ` Profitez jusqu'à ${discountValue}% de réduction en plus.`;
                     } else {
                         discountText = ` Enjoy up to ${discountValue}% off.`;
                     }
@@ -171,13 +190,11 @@ export function generarTarjetasProductos(category, site, delegation, idSlider) {
                 // cardContent.appendChild(pPriceDetails);
                 // cardContent.appendChild(pPrice);
                 cardContent.appendChild(pDiscount);
-                
                 cardLink.appendChild(img);
                 cardLink.appendChild(cardContent);
-                
                 cardsSlider.appendChild(cardLink);
             });
-            
+
             // Inicializar slick slider después de agregar todos los elementos
             
             $('#'+idSlider).slick({
